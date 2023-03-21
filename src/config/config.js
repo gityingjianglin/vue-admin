f618aaf0313137d3fe4564cc77061e40b4cb6139
gityingjianglin <yingjianglin123@163.com>
2023-03-21 10:09:13 +0800

    更新模板接入wlkjUtils类库

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@
  */
 let config = {
   namespace: 'vue-admin', // 必须设置，项目命名空间，以gitlab项目标识串为准，为解决同一个域名下面的本地存储问题，本地存储一律加上项目namespace-appId
-  openUserCenter: true, // 是否开启账户中心登录
+  openUserCenter: false, // 是否开启账户中心登录
   clientCode: 'FA000801', // euaf平台客户端code，用于换取euaf平台cliengId（非集团账户中心clientId）
   devUserCenterInfo: {
     localUrl: 'http://127.0.0.1'
